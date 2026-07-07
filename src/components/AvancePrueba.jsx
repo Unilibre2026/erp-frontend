1f559df01a158550d3d885ade03d2bda905c0d3c
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-07 08:34:24 -0500

    incluyendo módulo de avance de prueba

---
+++ src/components/AvancePrueba.jsx
@@ -0,0 +1,12 @@
+import "./AvancePrueba.css";
+
+function AvancePrueba() {
+  return (
+    <div className="avance-prueba">
+      <h2>Avance por prueba</h2>
+      <p>Módulo en construcción...</p>
+    </div>
+  );
+}
+
+export default AvancePrueba;
