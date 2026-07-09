1a0994bcf8c2936b413f1acf4f64b082d9a709fd
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-09 10:28:12 -0500

    recodificando consulta avance general

--- src/components/ConsultaAvanceGeneral.jsx
+++ src/components/ConsultaAvanceGeneral.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ConsultaAvanceGeneral.css";
 
 const API_URL = "https://erp-unilibre-production.up.railway.app";
