c94921c8d56631009463cf63bd91a7fd415c97e7
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-09 13:43:17 -0500

    prueba de exportar excel

--- src/utils/ExportadorExcel.js
+++ src/utils/ExportadorExcel.js
@@ -1,5 +1,35 @@
+import ExcelJS from "exceljs";
+import { saveAs } from "file-saver";
+
 export async function exportarAvanceGeneral() {
 
-    console.log("Exportador Excel listo");
+    // Crear libro
+    const workbook = new ExcelJS.Workbook();
+
+    workbook.creator = "ERP Universidad Libre";
+    workbook.lastModifiedBy = "ERP Universidad Libre";
+    workbook.created = new Date();
+
+    // Crear hoja
+    const worksheet = workbook.addWorksheet("Avance General");
+
+    // Texto de prueba
+    worksheet.getCell("A1").value = "UNIVERSIDAD LIBRE";
+
+    worksheet.getCell("A2").value = "CONSULTA AVANCE GENERAL";
+
+    worksheet.getCell("A3").value = "Exportación en construcción";
+
+    // Generar archivo
+
+    const buffer = await workbook.xlsx.writeBuffer();
+
+    saveAs(
+
+        new Blob([buffer]),
+
+        "Prueba.xlsx"
+
+    );
 
 }
