2b5e865b259e70a1df73f96b22213e2f10af4b00
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-24 15:09:50 -0500

    recoonfigurando consultas nuevas

--- src/components/ConsultaAvanceGeneral.jsx
+++ src/components/ConsultaAvanceGeneral.jsx
@@ -735,28 +735,36 @@ const obtenerTotalPreAprobadosCiudad = (ciudad) => {
                         ROLES
                 ========================== */}
 
-                {
+               {
+    roles.map((rol) => (
 
-                    roles.map((rol) => (
+        <React.Fragment key={rol}>
 
-    <React.Fragment key={rol}>
+            <td className="dato requerido">
+                {obtenerCantidad(ciudad, rol)}
+            </td>
 
-        <td className="dato total">
-            <strong>{obtenerTotalRol(rol)}</strong>
-        </td>
+            <td className="dato reclutado">
+                {obtenerReclutados(ciudad, rol)}
+            </td>
 
-        <td className="dato total">
-            <strong>{obtenerTotalReclutadosRol(rol)}</strong>
-        </td>
+            <td className="dato pre-aprobado">
+                {obtenerPreAprobados(ciudad, rol)}
+            </td>
 
-        <td className="dato total fin-rol">
-            <strong>0,0%</strong>
-        </td>
+            <td className="dato aprobado">
+                0
+            </td>
 
-    </React.Fragment>
+            <td className="dato porcentaje fin-rol">
+                0,0%
+            </td>
 
-))
-                }
+        </React.Fragment>
+
+    ))
+}
+                
 
                 <td className="dato total">
 
