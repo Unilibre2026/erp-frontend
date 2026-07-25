2fe358225b8fe24e7dcec67c7df86b8d5b488ca4
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-25 18:53:46 -0500

    validando prueba inicial de tabla VRM

--- src/components/AprobacionVRM.jsx
+++ src/components/AprobacionVRM.jsx
@@ -50,28 +50,73 @@ function AprobacionVRM() {
 
           <thead>
 
-            <tr>
+  <tr>
 
-              <th>N. Novedad</th>
-              <th>Validación</th>
-              <th>Justificación</th>
-              <th>Fecha novedad</th>
-              <th>Convocatoria</th>
-              <th>Eje / Indicador</th>
-              <th>Nivel</th>
-              <th>Rol</th>
-              <th>Nombres y apellidos experto</th>
-              <th>Documento</th>
-              <th>Perfil requerido</th>
-              <th>¿Cuenta con experiencia?</th>
+    <th style={{ minWidth: "90px" }}>N. Novedad</th>
 
-            </tr>
+    <th style={{ minWidth: "170px" }}>Validación</th>
 
-          </thead>
+    <th style={{ minWidth: "180px" }}>Justificación</th>
 
+    <th style={{ minWidth: "150px" }}>Fecha novedad</th>
+
+    <th style={{ minWidth: "220px" }}>Convocatoria</th>
+
+    <th style={{ minWidth: "260px" }}>Eje / Indicador</th>
+
+    <th style={{ minWidth: "100px" }}>Nivel</th>
+
+    <th style={{ minWidth: "180px" }}>Rol</th>
+
+    <th style={{ minWidth: "260px" }}>Nombres y apellidos experto</th>
+
+    <th style={{ minWidth: "140px" }}>Documento</th>
+
+    <th style={{ minWidth: "420px" }}>Perfil requerido</th>
+
+    <th style={{ minWidth: "180px" }}>
+      ¿Cuenta con experiencia en la entidad?
+    </th>
+
+  </tr>
+
+</thead>
           <tbody>
 
-          </tbody>
+<tr>
+
+<td>185</td>
+
+<td>
+    <button className="btn-aprobar">Aprobado</button>
+    <button className="btn-rechazar">No aprobado</button>
+</td>
+
+<td></td>
+
+<td>22/07/2026 13:52</td>
+
+<td>PRÁCTICAS ACUEDUCTO</td>
+
+<td>TEC_GUA1_Guardabosques_TÉCNICO</td>
+
+<td>TÉCNICO</td>
+
+<td>Juez / Aplicador</td>
+
+<td>JOSE GERMAN MENDEZ PARRA</td>
+
+<td>80820433</td>
+
+<td>
+Ingeniero forestal con 2 años de experiencia en el eje evaluativo.
+</td>
+
+<td>NO APLICA</td>
+
+</tr>
+
+</tbody>
 
         </table>
 
