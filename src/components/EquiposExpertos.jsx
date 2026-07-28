b9fac9f6eb44c5d1ecfca2603968fa5c7963789e
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-28 16:33:36 -0500

    incluyendo módulo de equipos

---
+++ src/components/EquiposExpertos.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./EquiposExpertos.css";
+
+function EquiposExpertos() {
+  return (
+    <div className="equipos-expertos">
+      <h2>Equipos de expertos</h2>
+      <p>Módulo en construcción...</p>
+    </div>
+  );
+}
+
+export default EquiposExpertos;
