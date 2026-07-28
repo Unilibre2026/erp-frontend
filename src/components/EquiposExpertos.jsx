49926fd3183e070f19ce5a31d717264ccdca68f1
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-28 16:36:20 -0500

    conctruyendo módulod e Equipos

--- src/components/EquiposExpertos.jsx
+++ src/components/EquiposExpertos.jsx
@@ -4,8 +4,27 @@ import "./EquiposExpertos.css";
 function EquiposExpertos() {
   return (
     <div className="equipos-expertos">
+
       <h2>Equipos de expertos</h2>
-      <p>Módulo en construcción...</p>
+
+      <div className="filtros-equipos">
+
+        <div className="grupo-filtro">
+          <label>Convocatoria</label>
+          <select>
+            <option>Seleccione...</option>
+          </select>
+        </div>
+
+        <div className="grupo-filtro">
+          <label>Ciudad</label>
+          <select>
+            <option>Seleccione...</option>
+          </select>
+        </div>
+
+      </div>
+
     </div>
   );
 }
