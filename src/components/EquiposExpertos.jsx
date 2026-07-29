b32a142d30aed78959a6bf8a1bc3ba77bb5f9993
Diego Alturo <supermotasunilibre@gmail.com>
2026-07-28 19:01:13 -0500

    corrigiendo filtros de equeipos

--- src/components/EquiposExpertos.jsx
+++ src/components/EquiposExpertos.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./EquiposExpertos.css";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://erp-unilibre-production.up.railway.app";
 
 function EquiposExpertos() {
 
